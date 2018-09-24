dc1c109f1e5d771950e48aed97f3f3269cf6822d
Dilusha Withanage <dilusha.withanage@gmail.com>
2018-09-24 22:09:36 +1000

    included optional execution delay

--- src/main/java/net/dilwit/springboot/ms/eurekaawareclient/controller/Controller.java
+++ src/main/java/net/dilwit/springboot/ms/eurekaawareclient/controller/Controller.java
@@ -1,13 +1,26 @@
 package net.dilwit.springboot.ms.eurekaawareclient.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class Controller {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Controller.class);
+
+    @Value("${execution.delayInSeconds}")
+    private int executionDelayInSeconds;
+
     @RequestMapping("/greeting")
-    public String greeting() {
+    public String greeting() throws InterruptedException {
+
+        LOGGER.info("EurekaAwareClient - greeting() invoked and processing delay: " + executionDelayInSeconds + " applied.");
+        TimeUnit.SECONDS.sleep(executionDelayInSeconds);
         return "Hello from EurekaAwareClient!";
     }
 
