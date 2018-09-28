27ccc5851661c67cb84893cc4c3a078f61098385
Dilusha Withanage <dilusha.withanage@gmail.com>
2018-09-28 21:22:23 +1000

    few updates to pass a message

--- src/main/java/net/dilwit/springboot/ms/eurekaawareclient/controller/Controller.java
+++ src/main/java/net/dilwit/springboot/ms/eurekaawareclient/controller/Controller.java
@@ -3,7 +3,9 @@ package net.dilwit.springboot.ms.eurekaawareclient.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -16,12 +18,14 @@ public class Controller {
     @Value("${execution.delayInSeconds}")
     private int executionDelayInSeconds;
 
-    @RequestMapping("/greeting")
-    public String greeting() throws InterruptedException {
 
-        LOGGER.info("EurekaAwareClient - greeting() invoked and processing delay: " + executionDelayInSeconds + " applied.");
+    @RequestMapping(value = "/greeting/{from}", method = RequestMethod.GET)
+    public String greeting(@PathVariable("from") String from) throws InterruptedException {
+
+        LOGGER.info("EurekaAwareClient - greeting "+ from +" invoked and processing delay: " + executionDelayInSeconds + " seconds applied.");
         TimeUnit.SECONDS.sleep(executionDelayInSeconds);
-        return "Hello from EurekaAwareClient!";
+
+        return "Hello " + from + "!!!";
     }
 
 }
